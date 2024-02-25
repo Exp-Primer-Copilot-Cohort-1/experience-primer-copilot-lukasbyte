5c28fb35a128d02746a7716b8f4f67a41925553a
Lukasbyte <136496784+lukasbyte@users.noreply.github.com>
2024-02-25 13:14:03 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember()
+{
+    var member = this;
+    member.skills = ["JavaScript", "HTML", "CSS"];
+    member.showSkills = function() {
+        member.skills.forEach(function(skill) {
+            console.log(skill);
+        });
+    };
+}
